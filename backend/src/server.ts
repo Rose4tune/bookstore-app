b3b62b43e3f3348cb623b103842e7de192f64747
Lee Ye Seo <rosefor2ne@gmail.com>
2024-12-18 10:57:23 +0900

    comment: 주석 정리

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,15 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const PORT = 5001;
 app.get("/api/books", (req: Request, res: Response) => {
   res.status(200).json([
     { id: 1, title: "책 제목", author: "저자 이름", stock: 10 },
     { id: 2, title: "제목", author: "이름", stock: 10 },
   ]);
 });
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
 
