aab43519947e8f1f2329b5ab648dcf61f4f75335
Lee Ye Seo <rosefor2ne@gmail.com>
2024-12-19 00:38:56 +0900

    fix: 프론트 테스트 코드 수정

--- frontend/__tests__/components/BookCard.test.tsx
+++ frontend/__tests__/components/BookCard.test.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import BookCard from "@/components/BookCard";
+import BookCard from "../../components/BookCard";
 
 describe("BookCard Component", () => {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
   it("renders the book title, author, and image", () => {
-    // Mock Props
     const book = {
+      id: 1,
       title: "테스트 책 제목",
       author: "테스트 저자",
-      imageUrl: "/uploads/test-image.jpg",
+      imageUrl: "/uploads/image.jpg",
     };
 
-    // Render the component
-    render(<BookCard title={book.title} author={book.author} imageUrl={book.imageUrl} />);
+    render(
+      <BookCard
+        id={book.id}
+        title={book.title}
+        author={book.author}
+        imageUrl={book.imageUrl}
+      />
+    );
 
-    // Assertions
     expect(screen.getByText(book.title)).toBeInTheDocument();
     expect(screen.getByText(book.author)).toBeInTheDocument();
+
     const image = screen.getByRole("img");
-    expect(image).toHaveAttribute("src", book.imageUrl);
     expect(image).toHaveAttribute("title", book.title);
   });
 });
